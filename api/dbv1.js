745ce8bfec0188d0bb05a4ee93bd11caa6388750
websiterafa <websitedbrafa@gmail.com>
2025-07-02 15:50:48 +0700

    Create dbv1.js

---
+++ api/dbv1.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).send("Only GET allowed");
+  }
+
+  const rawUrl = "https://raw.githubusercontent.com/websiterafa/Dbv10/refs/heads/main/dbv10.json";
+
+  try {
+    const response = await fetch(rawUrl);
+
+    if (!response.ok) {
+      throw new Error("Gagal ambil data dari GitHub");
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({
+      message: "❌ Gagal mengambil data",
+      error: err.message,
+    });
+  }
+}
