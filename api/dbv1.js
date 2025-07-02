778dcbfaa0309ee580afe842bc398e89ca26e497
websiterafa <websitedbrafa@gmail.com>
2025-07-02 16:51:15 +0700

    Update dbv1.js

--- api/dbv1.js
+++ api/dbv1.js
@@ -1,16 +1,9 @@
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).send("Only GET allowed");
-  }
-
   const rawUrl = "https://raw.githubusercontent.com/websiterafa/Dbv10/refs/heads/main/dbv10.json";
 
   try {
     const response = await fetch(rawUrl);
-
-    if (!response.ok) {
-      throw new Error("Gagal ambil data dari GitHub");
-    }
+    if (!response.ok) throw new Error("Gagal ambil data");
 
     const data = await response.json();
     res.status(200).json(data);
